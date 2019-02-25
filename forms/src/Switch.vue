c8d48efd910d6a04ce28e49567646066bc9f9c10
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-02-25 17:01:32 +0200

    working on forms

---
+++ forms/src/Switch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div id="on" @click="isOn = true" :class="{active: isOn}">On</div>
+    <div id="off" @click="isOn = false" :class="{active: !isOn}">Off</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      isOn: true
+    };
+  }
+};
+</script>
+<style scoped>
+#on,
+#off {
+  width: 40px;
+  height: 20px;
+  padding: 2px;
+  background-color: lightgray;
+  display: inline-block;
+  margin: 10px -2px;
+  box-sizing: content-box;
+  cursor: pointer;
+  text-align: center;
+}
+#on:hover,
+#on.active {
+  background-color: lightgreen;
+}
+#off:hover,
+#off.active {
+  background-color: lightcoral;
+}
+</style>
