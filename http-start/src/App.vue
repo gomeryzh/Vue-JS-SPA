f95aafb87a74399a978d0df0594b2a1f63ac7b63
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-02-27 22:49:07 +0200

    basic http requests work added

--- http-start/src/App.vue
+++ http-start/src/App.vue
@@ -11,7 +11,19 @@
           <label>Email</label>
           <input class="form-control" type="text" v-model="user.email">
         </div>
+        <input type="text" class="form-control" v-model="node">
+        <br>
+        <br>
         <button class="btn btn-primary" @click="submit">Submit</button>
+        <button class="btn btn-primary" @click="fetchData">Get data</button>
+        <br>
+        <ul class="list-group">
+          <li
+            class="list-group-item"
+            v-for="u in users"
+            :key="u + Math.random()"
+          >{{u.userName}} - {{u.email}}</li>
+        </ul>
       </div>
     </div>
   </div>
@@ -24,13 +36,57 @@ export default {
       user: {
         userName: "",
         email: ""
-      }
+      },
+      users: [],
+      resource: {},
+      node: "users"
     };
   },
+
   methods: {
     submit() {
-      console.log(this.user);
+      // this.$http.post("users.json", this.user).then(
+      //   response => {
+      //     console.log(response);
+      //   },
+      //   error => console.log(error)
+      // );
+      // this.resource.save({}, this.user);
+      this.resource.saveAlternative(this.user);
+    },
+    fetchData() {
+      // this.$http
+      //   .get("users.json")
+      // .then(response => {
+      //   return response.json();
+      // })
+      // .then(users => {
+      //   let usersArr = [];
+      //   for (let key in users) {
+      //     usersArr.push(users[key]);
+      //   }
+      //   this.users = usersArr;
+      // });
+      this.resource
+        .getData({ node: this.node })
+        .then(response => {
+          return response.json();
+        })
+        .then(users => {
+          let usersArr = [];
+          for (let key in users) {
+            usersArr.push(users[key]);
+          }
+          this.users = usersArr;
+        });
     }
+  },
+  created() {
+    const customAction = {
+      saveAlternative: { method: "POST", url: "alternative.json" },
+      getData: { method: "GET" }
+    };
+    this.resource = this.$resource("{node}.json", {}, customAction);
   }
 };
 </script>
