ce0cd7ca78a0b9024dde03bfdceeab6189720be7
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-02-28 22:31:40 +0200

    start with Vuex

---
+++ Vuex-state-managment/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Vuex</h1>
+        <app-result></app-result>
+        <hr>
+        <app-second-result></app-second-result>
+        <br>
+        <app-counter></app-counter>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+import Result from "./components/Result.vue";
+import SecondResult from "./components/SecondResult.vue";
+
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+  components: {
+    appCounter: Counter,
+    appResult: Result,
+    appSecondResult: SecondResult
+  }
+};
+</script>
+
