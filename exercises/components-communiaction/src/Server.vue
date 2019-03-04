60e2492aa3a56d155c94079db22fdf48c26ab28c
Volodymyr Omelianiuk <ovf.1990@gmail.com>
2019-03-04 22:34:39 +0200

    some practice added

---
+++ exercises/components-communiaction/src/Server.vue
@@ -0,0 +1,26 @@
+<template>
+  <li
+    class="list-group-item"
+    style="cursor: pointer"
+    @click="selectedServer"
+  >Server #{{ server.id }}</li>
+</template>
+
+<script>
+import { serverBus } from "./main.js";
+
+export default {
+  props: ["server"],
+  data() {
+    return {};
+  },
+  methods: {
+    selectedServer() {
+      serverBus.$emit("selectedServer", this.server);
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ exercises/components-communiaction/src/ServerDetails.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-xs-12 col-sm-6">
+    <p v-if="!server">Please select a Server</p>
+    <p v-else>Server #{{server.id}} selected. Server status: {{server.status}}</p>
+    <hr>
+    <button @click="resetStatus">Change to Normal status</button>
+  </div>
+</template>
+
+<script>
+import { serverBus } from "./main.js";
+
+export default {
+  data() {
+    return {
+      server: null
+    };
+  },
+  methods: {
+    resetStatus() {
+      this.server.status = "Normal";
+    }
+  },
+  created() {
+    serverBus.$on("selectedServer", server => {
+      this.server = server;
+    });
+  }
+};
+</script>
+
+<style scoped>
+</style>
